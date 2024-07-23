cc0581c59357d6b5fb070d80f5b89efac25d1da6
Ahmed Mafaz <33057319+ahmedmafaz@users.noreply.github.com>
2024-07-23 23:48:00 +0500

    Update env.js

--- src/core/env.js
+++ src/core/env.js
@@ -116,13 +116,13 @@ const defaults = new Map(
     // secondary doh upstream
     CF_DNS_RESOLVER_URL_2: {
       type: "string",
-      default: "https://0ms.dev/dns-query",
+      default: "https://freedns.controld.com/p0",
     },
 
      // tertiary doh upstream
     CF_DNS_RESOLVER_URL_3: {
       type: "string",
-      default: "https://dns.google/dns-query",
+      default: "https://dns.nextdns.io",
     },
     // upstream recursive rethinkdns resolver running on Fly.io
     MAX_DNS_RESOLVER_URL: {
